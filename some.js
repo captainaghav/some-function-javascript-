21716aa8b209a9a03a8660f5986ed536dec4ebcd
Shriram agahv <62254731+captainaghav@users.noreply.github.com>
2022-09-23 18:16:39 +0530

    some function code in javascript
    some()

---
+++ some.js
@@ -0,0 +1,12 @@
+//some function in js
+<script>
+        var ages = [12, 34, 22];
+        var b = ages.some(checkadult);
+        document.write(b + "<br> <br> ");
+
+        function checkadult(age) {
+            return age >= 18;
+        }
+
+        //ar ages =[20,22,3    we need the place whare you are gettinsion of o
+    </script>
